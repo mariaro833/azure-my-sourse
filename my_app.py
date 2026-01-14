bcf34eaf2a3668d7431b8fccde5426754bbbe89c
MariAro833 <masha.rozhnova@gmail.com>
2026-01-14 10:49:28 +0200

    upodated for production

--- my_app.py
+++ my_app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Maria's Azure Web Server!\n"
+    return render_template('index.html')
 
 if __name__ == "__main__":
+    # For local development
     app.run(debug=True)
+else:
+    # For production (Azure/Gunicorn)
+    port = int(os.environ.get('PORT', 8000))
